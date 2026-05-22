fcab1cbadf6b710c845ab2c0724c8038b9f03d65
PmpPiyas <princemahmudpiyas@gmail.com>
2026-05-23 00:26:45 +0600

    add: data embed & pinecone stored

--- src/common/config/env.config.ts
+++ src/common/config/env.config.ts
@@ -4,6 +4,10 @@ const envSchema = z.object({
   PORT: z.string().min(1, 'PORT is required'),
   MODEL: z.string().min(1, 'MODEL is required'),
   APIKEY: z.string().min(1, 'APIKEY is required'),
+  BASE_URL: z.string().min(1, 'BASE_URL is required'),
+  EMBEDDING_MODEL: z.string().min(1, 'EMBEDDING_MODEL is required'),
+  PINECONE_API_KEY: z.string().min(1, 'PINECONE_API_KEY is required'),
+  PINECONE_INDEX_NAME: z.string().min(1, 'PINECONE_INDEX_NAME is required'),
 });
 
 const parsedEnv = envSchema.parse(process.env);
@@ -11,5 +15,9 @@ const parsedEnv = envSchema.parse(process.env);
 export const ENV = {
   PORT: parsedEnv.PORT,
   MODEL: parsedEnv.MODEL,
+  EMBEDDING_MODEL: parsedEnv.EMBEDDING_MODEL,
   APIKEY: parsedEnv.APIKEY,
+  BASE_URL: parsedEnv.BASE_URL,
+  PINECONE_API_KEY: parsedEnv.PINECONE_API_KEY,
+  PINECONE_INDEX_NAME: parsedEnv.PINECONE_INDEX_NAME,
 };
--- src/modules/rag/rag.service.ts
+++ src/modules/rag/rag.service.ts
@@ -1,6 +1,10 @@
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
 import { Injectable } from '@nestjs/common';
 import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
+import { ENV } from 'src/common/config/env.config';
+import { OllamaEmbeddings } from '@langchain/ollama';
+import { Pinecone } from '@pinecone-database/pinecone';
+import { PineconeStore } from '@langchain/pinecone';
 
 @Injectable()
 export class RagService {
@@ -17,13 +21,32 @@ export class RagService {
     const slittedDocs = await splitter.splitDocuments(docs);
 
     const cleanedDocs = slittedDocs.map((doc) => ({
-      content: doc.pageContent,
-      source: doc.metadata.source,
-      pdf: {
-        name: doc.metadata.pdf.info.Title || 'Unknown',
-        page: doc.metadata.loc?.pageNumber,
+      pageContent: doc.pageContent,
+      metadata: {
+        source: doc.metadata.source,
+        pdf: {
+          name: doc.metadata.pdf.info.Title || 'Unknown',
+          page: doc.metadata.loc?.pageNumber,
+        },
       },
     }));
+
+    const embeddings = new OllamaEmbeddings({
+      model: 'nomic-embed-text:latest',
+      baseUrl: 'http://localhost:11434',
+    });
+
+    const vector = await embeddings.embedQuery('Hello world');
+
+    const pinecone = new Pinecone({
+      apiKey: ENV.PINECONE_API_KEY,
+    });
+
+    const pineconeIndex = pinecone.Index(ENV.PINECONE_INDEX_NAME);
+
+    await PineconeStore.fromDocuments(cleanedDocs, embeddings, {
+      pineconeIndex,
+    });
 
     return {
       success: true,
