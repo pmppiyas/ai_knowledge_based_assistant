45361c321ab7c4b3c691257b75450be118348c3d
PmpPiyas <princemahmudpiyas@gmail.com>
2026-06-06 13:21:49 +0600

    add: cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { ENV } from 'src/common/config/env.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['https://pmppiyas.vercel.app/'],
+    methods: ['GET'],
+    credentials: true,
+  });
+
   await app.listen(ENV.PORT);
   console.log(`Server is running on port ${ENV.PORT}`);
 }
